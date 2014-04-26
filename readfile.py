c808e4d84cc788c945b08db7f0ba1a0dff5ce4de
Andreas Söderlund <andreas.j.soderlund@gmail.com>
2014-04-26 19:01:05 +0200

    Added new python file

---
+++ readfile.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 26 18:53:16 2014
+
+@author: andreas
+"""
+from __future__ import division
+import zipfile as zf
+#This code will open an odt file and read the part 'content.xml'
+
+test = zf.ZipFile('a.-alcubilla_en.odt')
+ary = test.read('content.xml')
