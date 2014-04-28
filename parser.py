c9cc875537f9fd22e3cc9b112c4f3aee767b2a3c
Andreas Söderlund <andreas.j.soderlund@gmail.com>
2014-04-28 23:22:57 +0200

    Added a function that finds the name of the respondent

--- parser.py
+++ parser.py
@@ -4,7 +4,7 @@
 # Author: Kamran Husain
 #
 
-import sys
+import os, sys
 import zipfile as zf
 from xml.sax import parse, ContentHandler
 
@@ -33,8 +33,6 @@ if __name__ == '__main__':
     for x in myNames: print x
 """
 
-import os, sys
-import zipfile
 import xml.dom.minidom
 
 class OdfReader:
@@ -73,14 +71,38 @@ class OdfReader:
         for s in range(len(self.text_in_paras)):
 #            print s.encode('utf-8')
             if name in self.text_in_paras[s]:
-               print self.text_in_paras[s+1].encode('utf-8'), self.text_in_paras[s+3].encode('utf-8') 
-               
+               print self.text_in_paras[s].encode('utf-8') 
+            
+def findName(odtfile, nametag='Name:'):
+    """
+    Finds the name of the respondent by searching for the string
+    nametag and returning the string after that.
+    """
+    zipodt = zf.ZipFile(odtfile)
+    cont = zipodt.read('content.xml')
+    doc = xml.dom.minidom.parseString(cont)
+    paras = doc.getElementsByTagName("text:p")
+
+    ans = ""
+    
+    for st in paras:
+        for txt in st.childNodes:
+            if txt.nodeType == txt.TEXT_NODE:
+                if nametag in txt.data:
+                    for cn in st.nextSibling.childNodes:
+                        if cn.nodeType == cn.TEXT_NODE:
+                            ans = ans + cn.data + " "
+                    #Remove print when done.
+                    print ans
+                    return ans
+    return "Counld not find the string {}".format(nametag)
+
 def findStyles(odtfile, styletag = 'style:text-underline-type'):
     """
     This function will search through the file 'content.xml' to find
     the styles that underline the text in the file.
     """
-    zipodt = zipfile.ZipFile(odtfile)
+    zipodt = zf.ZipFile(odtfile)
     cont = zipodt.read('content.xml')
     doc = xml.dom.minidom.parseString(cont)
     
@@ -117,7 +139,7 @@ if __name__ == '__main__':
     """
     filename = 'input/TMP/a.-alcubilla_en.odt' #sys.argv[0] 
     phrase =  'Name:'#sys.argv[1]
-    findStyles(filename)
+    e = findName(filename)
     """
     if zipfile.is_zipfile(filename):
         myodf = OdfReader(filename) # Create object.
