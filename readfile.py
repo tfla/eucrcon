bfdc9c2a9a71a085f99d813862072be17c53fb91
Andreas Söderlund <andreas.j.soderlund@gmail.com>
2014-04-29 17:46:55 +0200

    Added function that finds the answer of the respondents. It doesn't work yet though, something about the encoding of the strings at line 175

--- readfile.py
+++ readfile.py
@@ -6,7 +6,12 @@ Created on Sat Apr 26 18:53:16 2014
 """
 from __future__ import division
 import zipfile as zf
-#This code will open an odt file and read the part 'content.xml'
+import sys
+from xml.sax import parse, ContentHandler
 
+#This code will open an odt file and read the part 'content.xml'
+"""
 test = zf.ZipFile('a.-alcubilla_en.odt')
 ary = test.read('content.xml')
+"""
+
