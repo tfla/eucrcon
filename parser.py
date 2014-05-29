81c7190ba1dd6106f08b67dcaa7dd4ac43377d76
Andreas Söderlund <andreass@maths.lth.se>
2014-05-29 17:13:53 +0200

    Updated parser.py with a commented method. Out of 25 tested .odt files the function could not find the answers in 9 of the files (fail rate of 36%)

--- parser.py
+++ parser.py
@@ -21,14 +21,12 @@ def findAttributeRecursive(element, tagName):
         if node.nodeType != node.TEXT_NODE and node.hasAttributes():
             if node.hasAttribute(tagName):
                 boo = True
-                print("Hello!")
             else:
                 boo = findAttributeRecursive(node, tagName)
         if boo: break
     else:
         if element.hasAttribute(tagName):
             boo = True
-            print("Hello!")
     return boo
        
 def getTextRecursive(element):
@@ -48,11 +46,16 @@ def getTextRecursive(element):
     return re.sub(" +", " ", joined).strip() # Remove duplicate, leading and trailing spaces
 
 def findUnderlinedRecursive(element, styles ,tag = 'text:style-name'):
+    """
+    This function searches through the element recursively to see if there exists
+    a tag with a value that exists in the list styles. If so, it returns True
+    and the text which has this attribute (in this case, the underlined text)
+    """
     if element.nodeName == 'text:p':
         paras = [element]
     else:
         paras = element.getElementsByTagName('text:p')
-    text = []
+    text = [] # This will collect all the text that is (in this case) underlined
     for st in paras:
         if st.hasAttribute(tag):
             if st.getAttribute(tag) in styles: # It checks if the style is among the styles that underlines the text      
@@ -61,21 +64,19 @@ def findUnderlinedRecursive(element, styles ,tag = 'text:style-name'):
                     if ch.nodeType == ch.TEXT_NODE:
                         paragraphText += ch.data
                 text.append(paragraphText)
-    if len(text) == 0:
+    if len(text) == 0: # If it didn't find any underlined text then len(text)==0 and False is returned
         return (False,' ')
     else:
         joined = " ".join(text)
         return (True,re.sub(" +", " ", joined).strip()) # Remove duplicate, leading and trailing spaces
                 
-def findName(odtFile, nameTag='Name:'):
+def findName(element, nameTag='Name:'):
     """
     Finds the name of the respondent by searching for the string
     nameTag and returning the string after that.
     """
-    zipodt = zf.ZipFile(odtFile)
-    cont = zipodt.read('content.xml')
-    doc = xml.dom.minidom.parseString(cont)
-    paras = doc.getElementsByTagName("text:p")
+    
+    paras = element.getElementsByTagName("text:p")
 
     ans = ""
     
@@ -86,22 +87,17 @@ def findName(odtFile, nameTag='Name:'):
                     for cn in st.nextSibling.childNodes:
                         if cn.nodeType == cn.TEXT_NODE:
                             ans += cn.data + " "
-                    #TODO: Remove print when done.
-                    print(ans)
                     return ans
     return "Could not find the string {}".format(nameTag)
 
-def findStyles(odtFile, styleTag='style:text-underline-type'):
+def findStyles(element, styleTag='style:text-underline-type'):
     """
     This function will search through the file 'content.xml' to find
     the styles that underline the text in the file.
     """
-    zipodt = zf.ZipFile(odtFile)
-    cont = zipodt.read('content.xml')
-    doc = xml.dom.minidom.parseString(cont)
     
     # Sees if there exists a child with tag office:automatic-styles
-    tempList = doc.getElementsByTagName("office:automatic-styles")    
+    tempList = element.getElementsByTagName("office:automatic-styles")    
     if len(tempList) == 1:
         allStyles = tempList[0]
     elif len(tempList) > 1:
@@ -126,15 +122,9 @@ def findStyles(odtFile, styleTag='style:text-underline-type'):
     
     return underlineList
 
-def countTag(odtFile, tag = 'text:continue-numbering'):
+def countTag(element, tag = 'text:continue-numbering'):
     
-    zipodt = zf.ZipFile(odtFile)
-    cont = zipodt.read('content.xml')
-    doc = xml.dom.minidom.parseString(cont)
-    
-#    paras = doc.getElementsByTagName('text:p')
-    paras = doc.getElementsByTagName('office:body')[0].childNodes[0].childNodes
-#    print(paras[0].childNodes)
+    paras = element.getElementsByTagName('office:body')[0].childNodes[0].childNodes
     
     counter = 0    
     for element in paras:
@@ -143,40 +133,47 @@ def countTag(odtFile, tag = 'text:continue-numbering'):
     return counter
     
     
-def findAnswers(odtFile, tag = 'text:continue-numbering'):
+def findAnswers(element, questions, openquest, styleTag='style:text-underline-type', tag = 'text:continue-numbering'):
+    """
+    This is the new updated findAnswers method, the old one is saved as findAnswers2.
+    this function will go through all the childNodes of the node 'office:body'
+    of the file content.xml. for each childNode it will see if there 'tag' is in
+    the node recursively. If there is it will increase questionNr by one. if questionNr
+    matches any of the numbers in 'questions' it will start seaching for an underlined
+    text. If it finds underlined text the underlined text will be regarded as an
+    answer and appended to the list 'ansList' if the function fins a new question
+    without having found an underlined text it will append 'NO COMMENT' to the
+    answer list. It will return a list containing the answers and possible free text.
+    As it is now, free text is not implemented yet but is just added as ' ' for
+    later compatibility.
+    """
+    underlinedStyles = findStyles(element, styleTag)
     
-    underlinedStyles = findStyles(odtFile)
-    
-    questions=list(range(14,34)) + list(range(35,78)) + list(range(79,87)) + [88, 89, 90, 92, 93, 94, 96, 97, 99]
-    openquest = [16,27,29,30,31,32,37,39,41]+list(range(43,50))+[51,52,53,57,58,59,60,62,63,65,66,68,69,70,71,75,76,77,81,84,85,86,88,90,93,97,99]
-    
-    zipodt = zf.ZipFile(odtFile)
-    cont = zipodt.read('content.xml')
-    doc = xml.dom.minidom.parseString(cont)
-    
-    paras = doc.getElementsByTagName('office:body')[0].childNodes[0].childNodes
-    numberOfCountAttribute = countTag(odtFile, tag)
-    if not numberOfCountAttribute == 100:
+    paras = element.getElementsByTagName('office:body')[0].childNodes[0].childNodes # Open all the childNodes of 'office:body'
+    numberOfCountAttribute = countTag(element, tag)
+    if not numberOfCountAttribute == 100: # If there are more or less 'text:continue-numbering' than 100 then this function will fail!
         print("Found {} number of text:continue-numbering which is not supported".format(numberOfCountAttribute))
     
-    ansList = []
-    questionNr = 0
-    foundQuest = False
+    ansList = [] # The answers.
+    questionNr = 0 # will range from 0 to 99 as each element of paras is gone through
+    foundQuest = False # If questionNr is in questions it turns to true
     for element in paras:
-#        print("Hello!")
         if findAttributeRecursive(element, tag):
             if questionNr in questions:
                 if foundQuest:
-                    ansList.append('NO COMMENT')
+                    ansList.append(['NO COMMENT', ' ']) #If nothing has been underlined then no free text should exist.
                 foundQuest = True
+                if questionNr in openquest:
+                    ansList.append(['OPEN QUESTION', ' ']) #implement a method to find the text added by the respondent!
+                    foundQuest = False # If it's an open question then it has found an answer.
             questionNr = questionNr + 1
         if foundQuest:
             ifFound, text = findUnderlinedRecursive(element, underlinedStyles)
             if ifFound:
-                ansList.append(text)
-                foundQuest = False
-    if len(ansList)<80:
-        ansList.append('NO COMMENT')
+                ansList.append([text, ' ']) # Add a method to find the text added by the respondent!
+                foundQuest = False # It has found an answer and start seaching for a new question.
+    if len(ansList)<80: # The last [open question] is not found in this function and thus the last NO COMMENT is added manually.
+        ansList.append(['OPEN QUESTION', ' ']) #implement a method to find the text added by the respondent!
     return ansList
                 
     
@@ -275,15 +272,59 @@ def parseOdfFile(filename):
                                     # paragraph text.
         myodf.findIt(phrase)        # find the phrase ...
     """
+    return False
 
+def parser(filename, questions=False, openquest=False, nameTag='Name:', styleTag='style:text-underline-type', numberingTag='text:continue-numbering'):
+    """
+    This is the main function that will open an .odt file and find the name of 
+    the respondent and the answers to the 80 questions.
+    nameTag is used to find the name of the respondent and will return the text
+    following the string in nameTag.
+    styleTag is used to find the answers. the text following the questions that
+    matches the styles containing styleTag will be regarded as answers.
+    numberingTag will be used to find the questions. Each time numberingTag is 
+    found it has the possibility of being a question as defined by the questions
+    list.
+    Returns the name of the respondent, the type of respondent (not implemented yet)
+    and the answers.
+    """
+    
+    # There are a total of 100 places in the file (hopefully) that have the part
+    # 'text:continue-numbering'. All of the questions will have these before.
+    # The list 'questions' will mark at wich number among the tags that each question
+    # appears.
+    if not questions:
+        questions = list(range(14,34)) + list(range(35,78)) + list(range(79,87)) + [88, 89, 90, 92, 93, 94, 96, 97, 99]
+        
+    # This is not used at the moment, but this is a list of where all the [open question]'s
+    # appears.
+    if not openquest:
+        openquest = [16,17,27,29,30,31,32,37,39,41]+list(range(43,50))+[51,52,53,57,58,59,60,62,63,65,66,68,69,70,71,75,76,77,81,84,85,86,88,90,93,97,99]
+    
+    
+    zipodt = zf.ZipFile(filename)
+    cont = zipodt.read('content.xml')
+    doc = xml.dom.minidom.parseString(cont)
+    
+    respondNam = findName(doc, nameTag)
+    
+    respondTyp = False # The type of the respondent (user/copyright holder/etc.). Not implemented yet!
+    respondAns = findAnswers(doc, questions, openquest, styleTag, numberingTag)
+    
+    return respondNam, respondTyp, respondAns
+    
 if __name__ == '__main__':
-    filename = 'input/TMP/a.nelles_en.odt'
+    filename = sys.argv[1]
 #    filename = 'input/users/a.-alcubilla_en.odt' #sys.argv[0]
 #    if not os.path.isfile(filename):
 #        print("ERROR: File {} not found. Make sure that input/users_en.zip is unzipped.".format(filename))
 #        sys.exit(1)
 #    parseOdfFile(filename)
-    ans = findAnswers(filename)
-    print(ans)
-    print(len(ans))
+    print(filename)
+    respondNam, respondTyp, respondAns = parser(filename)
+    print(respondNam)
+    print( )
+    for i in range(len(respondAns)):
+        print('Question {}: {}').format(i+1, respondAns[i])
+    print("Number of found answers: {}").format(len(respondAns))
     
